81814576ae171383ac4794764a0f5345758404bb
haotian <ritou11@gmail.com>
2018-06-05 23:44:01 +0800

    eslint

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const express = require('express');
 const wechat = require('./wechat');
 const config = require('./config');
 
-var app = express();
+const app = express();
 
-var wechatApp = new wechat(config);
+const wechatApp = new wechat(config);
 
-app.get('/wechat-api', function(req, res) { //微信验证请求
-    console.log('Received Authentication Request.')
-    wechatApp.auth(req, res);
+app.get('/wechat-api', (req, res) => { // 微信验证请求
+  console.log('Received Authentication Request.');
+  wechatApp.auth(req, res);
 });
 
-app.post('/wechat-api', function(req, res) {
-    wechatApp.handleMsg(req, res);
+app.post('/wechat-api', (req, res) => {
+  wechatApp.handleMsg(req, res);
 });
 
-app.get('/wechat-api/menu', function(req, res) { //强制刷新菜单接口
-    if (req.query.refresh === 'root') {
-        wechatApp.setMenu();
-        wechatApp.getAccessToken();
-    }
+app.get('/wechat-api/menu', (req, res) => { // 强制刷新菜单接口
+  if (req.query.refresh === 'root') {
+    wechatApp.setMenu();
+    wechatApp.getAccessToken();
+  }
 });
 
-app.get('/saishi', function(req, res) { //获取赛事详情
-    res.send('Success');
-})
+app.get('/saishi', (req, res) => { // 获取赛事详情
+  res.send('Success');
+});
 
 app.listen(8000);
-console.log('Server listening on 8000......')
+console.log('Server listening on 8000......');
