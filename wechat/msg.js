50bf787cf80a73d248d6c0513f0a564cc750aad2
haotian <ritou11@gmail.com>
2018-06-09 20:10:25 +0800

    pic reply; but stupid wechat donot give me auth to media

--- wechat/msg.js
+++ wechat/msg.js
@@ -1,17 +1,35 @@
+const _ = require('lodash');
+
 /**
  * 回复文本消息
  * @param {String} toUser 接收用户
  * @param {String} fromUser 发送用户
  * @param {String}  content 发送消息
  */
-exports.txtMsg = function(toUser, fromUser, content) {
-    var xmlContent = "<xml><ToUserName><![CDATA[" + toUser + "]]></ToUserName>";
-    xmlContent += "<FromUserName><![CDATA[" + fromUser + "]]></FromUserName>";
-    xmlContent += "<CreateTime>" + new Date().getTime() + "</CreateTime>";
-    xmlContent += "<MsgType><![CDATA[text]]></MsgType>";
-    xmlContent += "<Content><![CDATA[" + content + "]]></Content></xml>";
-    return xmlContent;
-}
+exports.txtMsg = (toUser, fromUser, content) => {
+  let xmlContent = `<xml><ToUserName><![CDATA[${toUser}]]></ToUserName>`;
+  xmlContent += `<FromUserName><![CDATA[${fromUser}]]></FromUserName>`;
+  xmlContent += `<CreateTime>${new Date().getTime()}</CreateTime>`;
+  xmlContent += '<MsgType><![CDATA[text]]></MsgType>';
+  xmlContent += `<Content><![CDATA[${content}]]></Content></xml>`;
+  return xmlContent;
+};
+
+/**
+ * 回复文本消息
+ * @param {String} toUser 接收用户
+ * @param {String} fromUser 发送用户
+ * @param {String}  mediaID 素材库中的图片ID
+ */
+exports.picMsg = (toUser, fromUser, mediaID) => {
+  let xmlContent = `<xml><ToUserName><![CDATA[${toUser}]]></ToUserName>`;
+  xmlContent += `<FromUserName><![CDATA[${fromUser}]]></FromUserName>`;
+  xmlContent += `<CreateTime>${new Date().getTime()}</CreateTime>`;
+  xmlContent += '<MsgType><![CDATA[image]]></MsgType>';
+  xmlContent += `<Content><![CDATA[${mediaID}]]></Content></xml>`;
+  return xmlContent;
+};
+
 
 /**
  * 回复图文消息
@@ -19,21 +37,21 @@ exports.txtMsg = function(toUser, fromUser, content) {
  * @param {String} fromUser 发送用户
  * @param {Array}  contentArr 图文信息集合
  */
-exports.graphicMsg = function(toUser, fromUser, contentArr) {
-    var xmlContent = "<xml><ToUserName><![CDATA[" + toUser + "]]></ToUserName>";
-    xmlContent += "<FromUserName><![CDATA[" + fromUser + "]]></FromUserName>";
-    xmlContent += "<CreateTime>" + new Date().getTime() + "</CreateTime>";
-    xmlContent += "<MsgType><![CDATA[news]]></MsgType>";
-    xmlContent += "<ArticleCount>" + contentArr.length + "</ArticleCount>";
-    xmlContent += "<Articles>";
-    contentArr.map(function(item, index) {
-        xmlContent += "<item>";
-        xmlContent += "<Title><![CDATA[" + item.Title + "]]></Title>";
-        xmlContent += "<Description><![CDATA[" + item.Description + "]]></Description>";
-        xmlContent += "<PicUrl><![CDATA[" + item.PicUrl + "]]></PicUrl>";
-        xmlContent += "<Url><![CDATA[" + item.Url + "]]></Url>";
-        xmlContent += "</item>";
-    });
-    xmlContent += "</Articles></xml>";
-    return xmlContent;
-}
+exports.graphicMsg = (toUser, fromUser, contentArr) => {
+  let xmlContent = `<xml><ToUserName><![CDATA[${toUser}]]></ToUserName>`;
+  xmlContent += `<FromUserName><![CDATA[${fromUser}]]></FromUserName>`;
+  xmlContent += `<CreateTime>${new Date().getTime()}</CreateTime>`;
+  xmlContent += '<MsgType><![CDATA[news]]></MsgType>';
+  xmlContent += `<ArticleCount>${contentArr.length}</ArticleCount>`;
+  xmlContent += '<Articles>';
+  _.forEach(contentArr, (item) => {
+    xmlContent += '<item>';
+    xmlContent += `<Title><![CDATA[${item.Title}]]></Title>`;
+    xmlContent += `<Description><![CDATA[${item.Description}]]></Description>`;
+    xmlContent += `<PicUrl><![CDATA[${item.PicUrl}]]></PicUrl>`;
+    xmlContent += `<Url><![CDATA[${item.Url}]]></Url>`;
+    xmlContent += '</item>';
+  });
+  xmlContent += '</Articles></xml>';
+  return xmlContent;
+};
